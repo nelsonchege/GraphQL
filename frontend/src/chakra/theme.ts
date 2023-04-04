22e6f7d315e31d5838f3620a2a6d079aef2a1b43
nelson <nelson3chege@gmail.com>
2023-04-05 02:02:26 +0300

    feat:setup chakra ui

---
+++ frontend/src/chakra/theme.ts
@@ -0,0 +1,24 @@
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+
+const configChakra: ThemeConfig = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+export const theme = extendTheme(
+  { configChakra },
+  {
+    colors: {
+      brand: {
+        100: "#3D84F7",
+      },
+    },
+    styles: {
+      global: () => ({
+        body: {
+          bg: "whiteAlpha.200",
+        },
+      }),
+    },
+  }
+);
