74d4bee95145a3ffb0c6f29202e072a7f2c40eac
nelson <nelson3chege@gmail.com>
2023-04-05 01:28:22 +0300

    update:finish setting up next auth

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -13,4 +13,5 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-})
+  secret: process.env.NEXTAUTH_SECRET,
+});
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,19 @@
-import { NextPage } from "next"
+import { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-const Home:NextPage =()=> {
+const Home: NextPage = () => {
+  const { data } = useSession();
+
+  console.log(data);
   return (
     <>
-      <div>hello world</div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>sign Out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>sign In</button>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
