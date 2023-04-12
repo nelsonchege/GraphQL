a4e7b80d7c2b5be67f00045320e2ca9bda9725d5
nelson <nelson3chege@gmail.com>
2023-04-13 01:11:09 +0300

    feat:setup backend and apollo server

--- frontend/src/graphql/apollo-client.ts
+++ frontend/src/graphql/apollo-client.ts
@@ -1,10 +1,4 @@
-import {
-  ApolloClient,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-  gql,
-} from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const httpLink = new HttpLink({
   uri: "http://localhost:4000/graphql",
